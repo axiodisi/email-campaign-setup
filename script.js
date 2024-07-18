0290ac4604ebaf08ff58ad763f79c91eb3576dab
axiodisi <140293854+axiodisi@users.noreply.github.com>
2024-07-18 10:18:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.getElementById('signup-form').addEventListener('submit', function(e) {
         email: email
     };
     
-    fetch('https://script.google.com/macros/s/AKfycbxmESn7TU7yWSYNlS63ZdoKpGSyuW5yG-UOHDQv4tvSOe5jc0FbPhcb_bl714MHAJJU/exec', {
+    fetch('https://script.google.com/macros/s/YOUR_SCRIPT_ID/exec', {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
